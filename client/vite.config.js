1ba2170eae325e817437c25b2531d3b0e6dd7c56
litvak.danila <litvak.danila@wb.ru>
2024-02-14 23:15:10 +0300

    3

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,15 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  server: {
+    proxy: {
+      '/ws': {
+        target: 'wss://dgsdgsdg.ru',
+        ws: true,
+        changeOrigin: true,
+        secure: true,
+      }
+    }
+  }
 })
